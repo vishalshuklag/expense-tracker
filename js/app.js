fa6600c9a6111782abf5147dd28186984be32bab
vishalshuklag <er_vishalshukla@outlook.com>
2021-03-24 02:13:55 +0530

    added edit & delete expense functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-class UI {
+class ExpenseTracker {
   constructor() {
     this.budgetFeedback = document.querySelector(".budget-feedback");
     this.expenseFeedback = document.querySelector(".expense-feedback");
@@ -40,7 +40,7 @@ class UI {
     const total = parseInt(this.budgetAmount.textContent) - expense;
     this.balanceAmount.textContent = total;
     if (total < 0) {
-      this.balance.clasadd("showRed");
+      this.balance.classList.add("showRed");
       this.balance.classList.remove("showGreen", "showBlack");
     } else if (total > 0) {
       this.balance.classList.add("showGreen");
@@ -53,7 +53,15 @@ class UI {
 
   //total expense
   totalExpense() {
-    const total = 400;
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce((acc, cur) => {
+        acc += cur.amount;
+        return acc;
+      }, 0);
+    }
+
+    this.expenseAmount.textContent = total;
     return total;
   }
 
@@ -61,21 +69,17 @@ class UI {
   submitExpenseForm() {
     const expenseTitle = this.expenseInput.value;
     const expenseValue = this.amountInput.value;
-    console.log(this.amountInput);
     if (expenseValue === "" || expenseTitle === "" || expenseValue < 0) {
       this.expenseFeedback.classList.add("show-item");
       this.expenseFeedback.innerHTML = `<p>
       Expense Title or Amount cannot be Empty or Negative
       </p>`;
-
       setTimeout(() => {
         this.expenseFeedback.classList.remove("show-item");
       }, 3000);
     } else {
       const amount = parseInt(expenseValue);
-      console.log(amount);
       this.expenseForm.reset();
-
       let expense = {
         id: this.itemID,
         title: expenseTitle,
@@ -83,9 +87,63 @@ class UI {
       };
       this.itemID++;
       this.itemList.push(expense);
-
-      console.log(this.itemList);
+      this.addExpense(expense);
+      this.showBalance();
     }
+  }
+
+  // add expense to the list at bottom
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+
+    div.innerHTML = `
+      <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+        <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+        <h5 class="expense-amount mb-0 list-item">₹${expense.amount}</h5>
+
+        <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+          <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+          <i class="fas fa-trash"></i>
+          </a>
+        </div>
+      </div>
+    `;
+    this.expenseList.appendChild(div);
+  }
+
+  // Edit expense
+  editExpense(element) {
+    let parent = element.parentElement.parentElement.parentElement;
+    let id = parseInt(element.dataset.id);
+    // remove element from dom
+    this.expenseList.removeChild(parent);
+
+    let expense = this.itemList.filter((item) => {
+      return item.id === id;
+    });
+    // show values to be edited in i/p fields
+    this.expenseInput.value = expense[0].title;
+    this.amountInput.value = expense[0].amount;
+    let tempList = this.itemList.filter((item) => {
+      return item.id !== id;
+    });
+    this.itemList = tempList;
+    this.showBalance();
+  }
+
+  // delete expense from list
+  deleteExpense(element) {
+    let parent = element.parentElement.parentElement.parentElement;
+    let id = parseInt(element.dataset.id);
+    this.expenseList.removeChild(parent);
+    this.itemList.splice(id, 1);
+    this.showBalance();
   }
 }
 
@@ -94,22 +152,27 @@ function eventListeners() {
   const expenseForm = document.getElementById("expense-form");
   const expenseList = document.getElementById("expense-list");
 
-  // new instance of UI CLASS
-  const ui = new UI();
+  // new instance of ExpenseTracker CLASS
+  const exp = new ExpenseTracker();
 
   // budget form submit
   budgetForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    ui.submitBudgetForm();
+    exp.submitBudgetForm();
   });
   // expense form submit
   expenseForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    ui.submitExpenseForm();
+    exp.submitExpenseForm();
   });
   // expense list event
   expenseList.addEventListener("click", (event) => {
-    event.preventDefault();
+    if (event.target.parentElement.classList.contains("edit-icon")) {
+      exp.editExpense(event.target.parentElement);
+    }
+    if (event.target.parentElement.classList.contains("delete-icon")) {
+      exp.deleteExpense(event.target.parentElement);
+    }
   });
 }
 
